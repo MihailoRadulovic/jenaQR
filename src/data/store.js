adc81b3ddeb9e5244b3d53adea5a6379db9aba3e
Mihailo Radulovic <mihailoradulovic711@gmail.com>
2025-06-19 07:58:55 +0200

    Izbacivanje krosasana

--- src/data/store.js
+++ src/data/store.js
@@ -4,9 +4,9 @@ import muffinCokolada2 from "../images/bakery/muffinCokolada2.jpg";
 import krofnaCokolada from "../images/bakery/krofnaCokolada.jpg";
 import krofnaKapri2 from "../images/bakery/krofnaKapri2.jpg";
 import kikiriki from "../images/bakery/kikiriki.jpg";
-import kroasan from "../images/bakery/kroasan.jpg";
-import kroasanPistac from "../images/bakery/kroasanPistac.jpg";
-import kroasanNutela from "../images/bakery/kroasanNutela.jpg";
+// import kroasan from "../images/bakery/kroasan.jpg";
+// import kroasanPistac from "../images/bakery/kroasanPistac.jpg";
+// import kroasanNutela from "../images/bakery/kroasanNutela.jpg";
 
 const store = [
   {
@@ -39,24 +39,24 @@ const store = [
     price: "130 RSD",
     photo: krofnaKapri2,
   },
-  {
-    id: 6,
-    category: "Kroasan",
-    price: "100 RSD",
-    photo: kroasan,
-  },
-  {
-    id: 7,
-    category: "Kroasan nutela",
-    price: "140 RSD",
-    photo: kroasanNutela,
-  },
-  {
-    id: 8,
-    category: "Kroasan nutela",
-    price: "150 RSD",
-    photo: kroasanPistac,
-  },
+  // {
+  //   id: 6,
+  //   category: "Kroasan",
+  //   price: "100 RSD",
+  //   photo: kroasan,
+  // },
+  // {
+  //   id: 7,
+  //   category: "Kroasan nutela",
+  //   price: "140 RSD",
+  //   photo: kroasanNutela,
+  // },
+  // {
+  //   id: 8,
+  //   category: "Kroasan nutela",
+  //   price: "150 RSD",
+  //   photo: kroasanPistac,
+  // },
   {
     id: 9,
     category: "Kikiriki (100g)",
